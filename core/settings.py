18856842346fef0e46f941fc44d8da0a90188d1c
Dhruv Patel <pateldhruv130401@gmail.com>
2023-02-26 18:06:53 +0530

    Model testing

--- core/settings.py
+++ core/settings.py
@@ -108,6 +108,21 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
