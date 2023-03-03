2ee27b355ceb89c6755c9cf344f17e1ae0815ff0
Dhruv Patel <pateldhruv130401@gmail.com>
2023-03-03 16:41:49 +0530

    Dynamic linking of product detail page

--- store/views.py
+++ store/views.py
@@ -13,3 +13,6 @@ def all_products(request):
 def product_details(request, slug):
     product = get_object_or_404(Product, slug=slug, in_stock=True)
     return render(request, 'store/products/detail.html', {'product': product})
+
+def category_list(request, category_slug):
+    pass
