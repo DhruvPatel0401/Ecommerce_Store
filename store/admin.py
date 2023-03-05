0c3ec4bf50db6a1a6276f62cb93a44f875d1f001
Dhruv Patel <pateldhruv130401@gmail.com>
2023-03-05 19:00:52 +0530

    Added comments in admin.py

--- store/admin.py
+++ store/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from .models import Category, Product
 
+#Registers model to admin panel 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug']
-    prepopulated_fields = {'slug':('name',)}
+    list_display = ['name', 'slug'] # Displays fields in list view in admin panel from model
+    prepopulated_fields = {'slug':('name',)} # Automatically generates slug from name field
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title','author','slug','price','in_stock','created','updated']
-    list_filter = ['in_stock','is_active']
-    list_editable = ['price','in_stock']
-    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ['in_stock','is_active'] # Add filters to the right sidebar of the list view
+    list_editable = ['price','in_stock']  # Allows editing directly in list view
+    prepopulated_fields = {'slug': ('title',)} 
