aac0717045d0c3e92e2751e1bad7e1399a43d3ec
Dhruv Patel <pateldhruv130401@gmail.com>
2023-03-05 16:46:36 +0530

    Added comments in models.py

--- store/models.py
+++ store/models.py
@@ -10,14 +10,20 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories'
 
+    #get_aboslute_url is called in template for dynamic linking. Return for generating the URL of the category_list view.
     def get_absolute_url(self):
         return reverse("store:category_list", args=[self.slug])
     
+    # for displaying the name of the category in the admin panel.
     def __str__(self):
         return self.name
     
 
 class Product(models.Model):
+    """
+    The related_name attribute of the category and created_by fields is used to create a reverse relationship between the Product and 
+    Category/User models, respectively.
+    """
     category = models.ForeignKey(Category,related_name='product', on_delete=models.CASCADE)
     created_by = models.ForeignKey(User, related_name='product_creater', on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
@@ -33,7 +39,7 @@ class Product(models.Model):
 
     class Meta:
         verbose_name_plural = 'Products'
-        ordering = ('-created',)
+        ordering = ('-created',)  #Ordering is used for default ordering for objects of particular model. 
 
     def get_absolute_url(self):
         return reverse('store:product_detail', args=[self.slug])
