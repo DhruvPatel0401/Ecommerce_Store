8600537b8bc639d2302153929debc1e32eacce06
Dhruv Patel <pateldhruv130401@gmail.com>
2023-03-09 22:58:16 +0530

    building the add-to-cart functionality

--- basket/basket.py
+++ basket/basket.py
@@ -9,9 +9,7 @@ class Basket():
 
     def __init__(self, request):
         self.session = request.session
-        print(self.session)
-        print(request.session)
         basket = self.session.get('skey')
         if 'skey' not in request.session:
-            basket = self.session['skey'] = {'number':1231231}
+            basket = self.session['skey'] = {}
         self.basket = basket
--- basket/views.py
+++ basket/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def basket_summary(request):
     return render(request, 'store/basket/summary.html')
+
+def basket_add(request):
+    pass
