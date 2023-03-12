0d992f21a9d219802272b3fa4132e2fad313a7dd
Dhruv Patel <pateldhruv130401@gmail.com>
2023-03-12 23:09:53 +0530

    Added comments

--- basket/context_processors.py
+++ basket/context_processors.py
@@ -1,5 +1,8 @@
 from .basket import Basket
 
-
+"""
+This code defines a context processor called basket that creates a new instance of the Basket class and makes it available as a context variable 
+for every template rendered for the request.
+"""
 def basket(request):
     return {'basket': Basket(request)}
