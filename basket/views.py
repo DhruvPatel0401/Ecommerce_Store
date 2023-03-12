466e85d95ebb035d95082eddf76c376b26f6ec80
Dhruv Patel <pateldhruv130401@gmail.com>
2023-03-12 23:17:17 +0530

    Added comments

--- basket/views.py
+++ basket/views.py
@@ -7,6 +7,12 @@ from .basket import Basket
 def basket_summary(request):
     return render(request, 'store/basket/summary.html')
 
+
+"""
+This function is called when the user clicks the "add to basket" button on a products single.html page. It retrieves the product ID and quantity 
+from the request's POST data, retrieves the corresponding Product object from the database using get_object_or_404, and adds the product to the 
+user's basket using the add method of the Basket object. Finally, it returns a JSON response with the updated quantity of items in the basket.
+"""
 def basket_add(request):
     basket = Basket(request)
     if request.POST.get('action') == 'post':
