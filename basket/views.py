13a68bf960feb299227f788ac7006284e36363f2
Dhruv Patel <pateldhruv130401@gmail.com>
2023-03-15 22:25:59 +0530

    updated delete ajax success response

--- basket/views.py
+++ basket/views.py
@@ -31,7 +31,10 @@ def basket_delete(request):
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('productid'))
         basket.delete(product=product_id)
-        response = JsonResponse({'Success': True})
+        
+        basketqty = basket.__len__()
+        baskettotal = basket.get_total_price()
+        response = JsonResponse({'subtotal': baskettotal, 'qty': basketqty})
         return response
 
 def basket_update(request):
@@ -43,7 +46,5 @@ def basket_update(request):
 
         basketqty = basket.__len__()
         baskettotal = basket.get_total_price()
-        print(basketqty)
-        print(baskettotal)
         response = JsonResponse({'qty': basketqty, 'subtotal': baskettotal})
         return response
