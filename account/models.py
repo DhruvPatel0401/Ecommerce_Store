ad561f57f9110b70ce56249b862c554023867aa9
Dhruv Patel <pateldhruv130401@gmail.com>
2023-03-21 22:54:53 +0530

    Created account app and UserBase class in model

---
+++ account/models.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
+from django_countries.fields import CountryField
+
+class UserBase(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(_('email address'), unique=True)
+    user_name = models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    about = models.TextField(_('about'), max_length=500, blank=True)
+    # Delivery details
+    country = CountryField()
+    postcode = models.CharField(max_length=12, blank=True)
+    address_line_1 = models.CharField(max_length=150, blank=True)
+    address_line_2 = models.CharField(max_length=150, blank=True)
+    town_city = models.CharField(max_length=150, blank=True)
+    # User Status
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    objects = CustomAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['user_name']
+
+    class Meta:
+        verbose_name = "Accounts"
+        verbose_name_pural = "Accounts"
+
+    def __str__(self):
+        return self.user_name
+    
