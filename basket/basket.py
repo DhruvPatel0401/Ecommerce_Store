2b839ac005af72e91da3dd1995019fd2c571ee79
Dhruv Patel <pateldhruv130401@gmail.com>
2023-03-23 19:21:15 +0530

    Added comments in basket.py

--- basket/basket.py
+++ basket/basket.py
@@ -8,11 +8,11 @@ can be inherited or overrided, as necessary.
 """
 class Basket():
 
-    """
-    The '__init__' method initializes the shopping basket by getting the session data and setting the 'basket' attribute to the value of the 'skey' 
-    key in the session data, or an empty dictionary if the skey key does not exist in the session.
-    """
     def __init__(self, request):
+        """
+        The '__init__' method initializes the shopping basket by getting the session data and setting the 'basket' attribute to the value of the 
+        'skey' key in the session data, or an empty dictionary if the skey key does not exist in the session.
+        """
         self.session = request.session
         basket = self.session.get('skey')
         if 'skey' not in request.session:
@@ -32,7 +32,12 @@ class Basket():
 
         self.save()
 
+
     def __iter__(self):
+        """
+        The method allows the class instance to be iterated over using a for loop, and it returns a generator object that yields a dictionary 
+        for each item in the basket.
+        """
         product_ids = self.basket.keys()
         products = Product.products.filter(id__in=product_ids)
         basket = self.basket.copy()
@@ -46,14 +51,14 @@ class Basket():
             yield item
 
     def update(self, product, qty):
-            """
-            Update values in session data
-            """
-            product_id = str(product)
+        """
+        Update values in session data
+        """
+        product_id = str(product)
 
-            if product_id in self.basket:
-                self.basket[product_id]['qty'] = qty
-            self.save()
+        if product_id in self.basket:
+            self.basket[product_id]['qty'] = qty
+        self.save()
 
     def __len__(self):
         """
