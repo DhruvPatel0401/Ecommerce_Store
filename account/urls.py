6e02b4e43cf97a4e4a563c59470359666557bf9e
Dhruv Patel <pateldhruv130401@gmail.com>
2023-04-03 19:59:01 +0530

    created reset status templated and modified url and password reset form template

--- account/urls.py
+++ account/urls.py
@@ -18,7 +18,9 @@ urlpatterns = [
                                                                 form_class=PwdResetForm), name='pwdreset'),
     path('password_reset_confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='account/user/password_reset_confirm.html',
                                                                                                 success_url='password_reset_complete/', 
-                                                                                                form_class=PwdResetConfirmForm)),
+                                                                                                form_class=PwdResetConfirmForm), name='password_reset_confirm'),
+    path('password_reset/password_reset_email_confirm/',
+        TemplateView.as_view(template_name="account/user/reset_status.html"), name='password_reset_done'),
     #User dashboard
     path('dashboard/',views.dashboard, name='dashboard'),
     path('profile/edit/', views.edit_details, name='edit_details'),
