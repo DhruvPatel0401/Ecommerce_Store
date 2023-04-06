ea66c728fb3a40af1e583a4ee2220b756a928953
Dhruv Patel <pateldhruv130401@gmail.com>
2023-04-06 12:44:06 +0530

    created basketview function in payment app. setup razorpay in home.html of payment app

--- payment/views.py
+++ payment/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from basket.basket import Basket
+
 @login_required
 def BasketView(request):
-    return render(request, "payment/home.html")
+
+    basket = Basket(request)
+    total = basket.get_total_price()
+
+    return render(request, "payment/home.html", {'total_amount': total})
