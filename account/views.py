48d20cc03c6d911d7bb3b092ac26e09ed9c132bc
Dhruv Patel <pateldhruv130401@gmail.com>
2023-04-21 17:33:33 +0530

    refactored account registration and password reset templates

--- account/views.py
+++ account/views.py
@@ -15,7 +15,7 @@ from orders.views import user_orders
 @login_required
 def dashboard(request):
     orders = user_orders(request)
-    return render(request, 'account/user/dashboard.html', {'orders': orders})
+    return render(request, 'account/dashboard/dashboard.html', {'orders': orders})
 
 @login_required
 def edit_details(request):
@@ -30,7 +30,7 @@ def edit_details(request):
     else:
         user_form = UserEditForm(instance=request.user)
 
-    return render(request, 'account/user/edit_details.html', {'user_form': user_form})
+    return render(request, 'account/dashboard/edit_details.html', {'user_form': user_form})
 
 @login_required
 def delete_user(request):
@@ -63,7 +63,7 @@ def account_register(request):
                 'token': account_activation_token.make_token(user),
             })
             user.email_user(subject=subject, message=message)
-            return HttpResponse('Registered succesfully and activation sent')
+            return render(request, 'account/registration/register_email_confirm.html', {'form': registerForm})
 
     else:
         registerForm = RegistrationForm()
