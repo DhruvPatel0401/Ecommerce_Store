e0df1706607ab94362bc35b47180326299a58572
Dhruv Patel <pateldhruv130401@gmail.com>
2023-05-11 23:59:20 +0530

    improved razorpay integration code

--- checkout/views.py
+++ checkout/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, JsonResponse
 from basket.basket import Basket
 
+from orders.models import Order, OrderItem
 from account.models import Address
 from .models import DeliveryOptions
 
@@ -59,23 +60,40 @@ def payment_selection(request):
         messages.success(request, "Please select address option")
         return HttpResponseRedirect(request.META["HTTP_REFERER"])
 
+    client = razorpay.Client(auth=(settings.RAZOR_KEY_ID, settings.RAZOR_KEY_SECRET))
+    basket = Basket(request)
+    amount = int(basket.get_total_price()*100)
+    
+    Data = {
+        "amount":  amount,
+        "currency": "INR",
+        "payment_capture": 1
+    }
+    payment = client.order.create(data=Data)
 
-    return render(request, "checkout/payment_selection.html", {})
+    print("*****************************")
+    print(payment)
+    print("*****************************")
+    return render(request, "checkout/payment_selection.html", {'payment': payment})
 
 @login_required
 def payment_complete(request):
-    client = razorpay.Client(auth=(settings.RAZOR_KEY_ID, settings.RAZOR_KEY_SECRET))
+
+    # order = Order.objects.create(
+    #     user_id=user_id,
+    #     full_name=response.result.purchase_units[0].shipping.name.full_name,
+    #     email=response.result.payer.email_address,
+    #     address1=response.result.purchase_units[0].shipping.address.address_line_1,
+    #     address2=response.result.purchase_units[0].shipping.address.admin_area_2,
+    #     postal_code=response.result.purchase_units[0].shipping.address.postal_code,
+    #     country_code=response.result.purchase_units[0].shipping.address.country_code,
+    #     total_paid=response.result.purchase_units[0].amount.value,
+    #     order_key=response.result.id,
+    #     payment_option="paypal",
+    #     billing_status=True,
+    # )
+    # order_id = order.pk
+
+    # print(order)
     
-    Data = {
-        "amount":  "5000",
-        "currency": "INR",
-        "receipt": "receipt#1",
-        "notes": {
-            "key1": "value3",
-            "key2": "value2"
-        }
-    }
-
-    client.order.create(data=Data)
-
     return JsonResponse("Payment completed!", safe=False)
