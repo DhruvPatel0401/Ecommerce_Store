7a3508b5e13463c7c4c6ae925ff5fa7fbc817c7c
Dhruv Patel <pateldhruv130401@gmail.com>
2023-05-16 13:11:40 +0530

    refactored project structure

---
+++ ecommerce/apps/catalogue/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, get_object_or_404
+
+from .models import Category, Product
+
+
+"""
+The Product.products.all() method call returns a queryset of all active products using the custom products manager for the Product model that 
+filters the queryset to only include active products.
+"""
+
+
+def product_all(request):
+    products = Product.objects.prefetch_related("product_image").filter(is_active=True)
+    return render(request, "catalogue/index.html", {"products": products})
+
+
+"""
+The product_details function takes a slug parameter in the URL and uses it to retrieve the corresponding product from the database using the 
+get_object_or_404 function. If the product exists and is in stock, the function renders the detail.html template with the product details.
+"""
+
+
+def product_details(request, slug):
+    product = get_object_or_404(
+        Product, slug=slug, is_active=True
+    )  # If you want to retrieve a single object based on a specific filter parameter and you want to raise a HTTP 404 exception if the object does not exist, you should use get_object_or_404. If you want to handle the DoesNotExist exception yourself, you should use Product.objects.get(slug=slug)
+    return render(request, "store/single.html", {"product": product})
+
+
+"""
+The category_list function takes a category_slug parameter in the URL and uses it to retrieve the corresponding category from the database using
+the get_object_or_404 function. The function then retrieves all the products in the given category using the Product.objects.filter method and
+renders them in the category.html template, along with the category details.
+"""
+
+
+def category_list(request, category_slug=None):
+    category = get_object_or_404(Category, slug=category_slug)
+    products = Product.objects.filter(
+        category__in=Category.objects.get(name=category_slug).get_descendants(include_self=True)
+    )
+    return render(request, "store/category.html", {"category": category, "products": products})
---
+++ ecommerce/settings.py
@@ -0,0 +1,120 @@
+from pathlib import Path
+import os, dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = "django-insecure-qcs&^c6o3y55*r4o3=g7&(iv%dma1vx4p_cge$2z_hf#x@veji"
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["yourdomain.com", "127.0.0.1"]
+
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "ecommerce.apps.catalogue",
+    "ecommerce.apps.account",
+    "ecommerce.apps.basket",
+    "ecommerce.apps.checkout",
+    "ecommerce.apps.orders",
+    "mptt",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "ecommerce.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "ecommerce.apps.catalogue.context_processors.categories",
+                "ecommerce.apps.basket.context_processors.basket",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "ecommerce.wsgi.application"
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+    # "default": dj_database_url.config(),
+}
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+STATIC_URL = "static/"
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+
+# Basket session ID
+BASKET_SESSION_ID = "basket"
+
+# Custom user model
+AUTH_USER_MODEL = "account.Customer"
+LOGIN_REDIRECT_URL = "/account/dashboard"
+LOGIN_URL = "/account/login"
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+# Razorpay information
+RAZOR_KEY_ID = os.getenv("RAZOR_KEY_ID")
+RAZOR_KEY_SECRET = os.getenv("RAZOR_KEY_SECRET")
+
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
