d5e1cb7d64b3bd8644e00ab74a135ed4559ca100
Dhruv Patel <pateldhruv130401@gmail.com>
2023-05-23 23:28:39 +0530

    tried django pytest

--- tests/catalogue/test_catalogue.py
+++ tests/catalogue/test_catalogue.py
@@ -1,16 +1,18 @@
 # from django.test import TestCase
 
+
 # class TestClass(TestCase):
 #     def test_hello_world(self):
 #         self.assertEqual("Hello", "Hello")
 
 import pytest
 
-@pytest.fixture()
-def test_fixture1():
-    print("Run each test")
-    return 1
 
-def test_hello_world(test_fixture1):
+def test_hello_world1(test_fixture1):
     print("function_fixture1")
+    assert test_fixture1 == 1
+
+
+def test_hello_world2(test_fixture1):
+    print("function_fixture2")
     assert test_fixture1 == 1
